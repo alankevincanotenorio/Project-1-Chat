498e20b54be4a77a25178c447e350f0d90bed499
AlanKevinCT <Alan.Kevin@ciencias.unam.mx>
2024-09-11 11:36:29 -0600

    El servidor tiene como direccion ip el localhost

--- src/mainServer.cpp
+++ src/mainServer.cpp
@@ -2,13 +2,12 @@
 #include "Server.cpp"
 
 int main(int argc, char* argv[]) {
-    if(argc != 3) {
+    if(argc != 2) {
         cerr  << "Please only execute the bin archive and insert the server port" << endl;
         return 0;
     }
-    string ip = argv[1];
-    int port = stoi(argv[2]);
-    unique_ptr<Server> server = make_unique<Server>(ip, port);
+    int port = stoi(argv[1]);
+    unique_ptr<Server> server = make_unique<Server>("localhost", port);
     server->initSocket();
     server->connectClient();
     return 0;
